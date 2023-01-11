b9deee7f583158fb32c499f130da21b14ac3f2ff
Aubis Sanchez <aubisst@gmail.com>
2023-01-11 15:37:25 -0400

    env variables

--- src/firebase/config.firebase.ts
+++ src/firebase/config.firebase.ts
@@ -2,12 +2,12 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: "interview-gp-challenge",
-  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID 
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
 
 export const app = initializeApp(firebaseConfig);
